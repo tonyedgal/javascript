35b24730a8e76bfe4c1715b23b0dd62b30042696
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-11-04 13:51:23 +0100

    feat: DOM manipulation

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const searchIndex = (numbers, target) => {
-  let tar
-  numbers
+  let tar;
+  number
     .sort((a, b) => a - b)
     .forEach((num, index) => {
       if (target === num) {
-        tar = numbers.indexOf(target)
+        tar = numbers.indexOf(target);
       } else if (target !== num) {
-        let newNumbers = numbers.concat(target)
-        newNumbers.sort((a, b) => a - b)
-        tar = newNumbers.indexOf(target)
+        let newNumbers = numbers.concat(target);
+        newNumbers.sort((a, b) => a - b);
+        tar = newNumbers.indexOf(target);
       }
-    })
-  return tar
-}
+    });
+  return tar;
+};
+
+// ES6 Promises
+
+let sam = new Promise(resolve, reject);
